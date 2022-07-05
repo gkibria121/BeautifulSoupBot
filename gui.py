3ca12ceb35b9cd427baa009f0cc01828e13473e4
gkibria121 <42410742+gkibria121@users.noreply.github.com>
2022-07-05 23:25:19 +0600

    Add files via upload

--- gui.py
+++ gui.py
@@ -9,38 +9,46 @@ def download():
     url = e.get()
     print(url)
     preferd= str(var.get())
-#    if  "season" in e.get().lower():
-#        preferd ="3"
+    print(preferd)
     choise= str(man.get())
+    if pix.get() ==1:
+        pixel = '1080p'
+    elif pix.get() ==2:
+        pixel = '720p'
+    elif pix.get() ==3:
+       pixel = 'HVEC 720p '
+    elif pix.get() ==4:
+       pixel = '480p'
     if preferd =='1':
-        if pix.get() ==1:
-            pixel = '1080p'
-        elif pix.get() ==2:
-            pixel = '720p'
-        elif pix.get() ==3:
-           pixel = 'HVEC 720p '
-        elif pix.get() ==4:
-           pixel = '480p'
-        mrun.run(url,choise,pixel)
+        if  "season" in e.get().lower():
+            preferd ="3"
+        else:
+            mrun.run(url,choise,pixel)
     if preferd == '2':
         global myLabel2,e2,getLink
         openbrowser(url)
         myLabel2 = Label(frame,text="Enter your prefered download link")
-        myLabel2.grid(row=20,column=1)
+        myLabel2.grid(row=30,column=1)
         e2 = Entry(frame)
-        e2.grid(row=14,column=2)
+        e2.grid(row=20,column=2)
         getLink = Button(frame,text="Get link!", command=lambda: selecteDownload(e2.get()))
         getLink.grid(row=20,column=3)
     if preferd =="3":
         if eors.get() == 1:
-            print("ep")
             if ep.get() ==1:
-                print("All episode selected")
-
+                print("All episode selected",pixel,choise)
+                season ="1"
+                episode= "all"
+                epwise.episodeSelect(choise,season,episode,pixel,url)
             elif ep.get()==2:
                 print("Latest episode selected")
+                season ="1"
+                episode= "latest"
+                epwise.episodeSelect(choise,season,episode,pixel,url)
         elif eors.get()==2:
             print("season")
+
+
 
 
 def eps():
@@ -49,6 +57,9 @@ def eps():
     p8 = Radiobutton(frame,text="latest", variable=ep, value=2)
     p7.grid(row=7,column=3)
     p8.grid(row=8,column=3)
+
+
+
 def season():
     try:
         p7.grid_remove()
@@ -59,9 +70,9 @@ def season():
 def manual():
     removeManual()
     if "season" not in e.get().lower():
-        global myLabel, myLabel1, gds, gdrive,p1,p2,p3,p4,p5,p6
+        global myLabel, myLabel1, gds, gdrive,p1,p2,p3,p4,p5,p6,b7,p8
 
-        myLabel = Label(frame,text="How do you want to download the file")
+        myLabel = Label(frame,text="How do you want to download the file?")
         myLabel.grid(row=4,column=2)
 
         gds = Radiobutton(frame,text="1. GDS", variable=man, value= 1)
@@ -70,7 +81,7 @@ def manual():
         gds.grid(row=5,column=2)
         gdrive.grid(row=6,column=2)
 
-        myLabel1 = Label(frame,text="What is the pixel")
+        myLabel1 = Label(frame,text="What is the pixel?")
         myLabel1.grid(row=7,column=2)
 
         p1 = Radiobutton(frame,text="1080p", variable=pix, value=1)
@@ -83,10 +94,36 @@ def manual():
         p4.grid(row=11,column=2)
     elif "season" in e.get().lower():
 
-        p5 = Radiobutton(frame,text="EP", variable=eors, value=1, command= eps )
-        p6 = Radiobutton(frame,text="Season", variable=eors, value=2, command = season)
-        p5.grid(row=4,column=2)
-        p6.grid(row=5,column=2)
+        myLabel = Label(frame,text="How do you want to download the file?")
+        myLabel.grid(row=4,column=2)
+
+        gds = Radiobutton(frame,text="1. GDS", variable=man, value= 1)
+        gdrive = Radiobutton(frame,text="2. GDrive",variable=man, value=2)
+
+        gds.grid(row=5,column=2)
+        gdrive.grid(row=6,column=2)
+
+        myLabel1 = Label(frame,text="What is the pixel?")
+        myLabel1.grid(row=7,column=2)
+
+        p1 = Radiobutton(frame,text="1080p", variable=pix, value=1)
+        p2 = Radiobutton(frame,text="720p", variable=pix, value=2)
+        p3 = Radiobutton(frame,text="720p HVEC", variable=pix, value=3)
+        p4 = Radiobutton(frame,text="480p", variable=pix, value=4)
+
+        p1.grid(row=8,column=2)
+        p2.grid(row=9,column=2)
+        p3.grid(row=10,column=2)
+        p4.grid(row=11,column=2)
+
+        p5 = Label(frame,text="EP wise or Season wise?")
+        p6 = Radiobutton(frame,text="EP", variable=eors, value=1, command= eps )
+        b7 = Radiobutton(frame,text="Season", variable=eors, value=2, command = season)
+
+        p5.grid(row=12,column=2)
+        p6.grid(row=13,column=2)
+        b7.grid(row=14,column=2)
+
         if str(eors.get()) == "1":
             print("Ep")
         elif str(eors.get()) =='2':
@@ -120,6 +157,10 @@ def removeManual():
         p8.grid_remove()
     except:
         pass
+    try:
+        b7.grid_remove()
+    except:
+        pass
 
 def custom():
     removeManual()
@@ -131,7 +172,10 @@ def custom():
 root= Tk()
 root.title("Py downloader")
 root.geometry("500x600")
-root.iconbitmap("mlwbd.ico")
+try:
+    root.iconbitmap("mlwbd.ico")
+except:
+    pass 
 #declaring variable
 var = IntVar()
 man = IntVar()
